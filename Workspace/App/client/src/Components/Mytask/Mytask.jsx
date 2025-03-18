76fff7cdcbe74cc81fa8afeb2ffec8a0b53f598b
NghiTran <nghi.forwork05@gmail.com>
2025-03-18 22:25:15 +0700

    mobile friendly my Task

--- Workspace/App/client/src/Components/Mytask/Mytask.jsx
+++ Workspace/App/client/src/Components/Mytask/Mytask.jsx
@@ -6,101 +6,87 @@ import threebears from "../../assets/threebears.jpg";
 const Mytask = () => {
   return (
     <div className="app-container">
-      <div className="mytask">
-      <div className="site-title">
-      {/* <img src={threebears} alt="logo"/> */}
-      {/* <Link to="/">My Task</Link> */}
-      </div>
-        {/* <nav>
-            
-          <ul>
-            <li><Link to="/">My Task</Link></li>
-            <li><Link to="/notifications">🔔 Notifications</Link></li>
-            <li><Link to="/goals">🎯 Goals</Link></li>
-          </ul>
-        </nav> */}
-        
-      </div>
+      
 
       {/* Main Content */}
       <div className="main-content">
         {/* Task Dashboard */}
-        <div className="header">
-            <h2>To Do or Not Marked Done</h2>
-          </div>
+          
         <div className="task-dashboard">
-          
-          <div className="task-list">
-           <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
+            <div className="header">
+                <h2>To Do or Not Marked Done</h2>
             </div>
+            <div className="task-list">
             <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot red"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot red"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot blue"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot green"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot green"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot green"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Nghĩa làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot yellow"></span> Gia Nghi làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+              <div className="task">
+                  <span className="dot green"></span> Gia Nghi làm hết
+                  <input type="checkbox" className="task-checkbox" />
+              </div>
+            
             </div>
-            <div className="task">
-                <span className="dot red"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot red"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot blue"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot green"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot green"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot green"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Nghĩa làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot yellow"></span> Gia Nghi làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-            <div className="task">
-                <span className="dot green"></span> Gia Nghi làm hết
-                 <input type="checkbox" className="task-checkbox" />
-            </div>
-          
-          </div>
         </div>
 
         {/* Calendar */}
