b10a24e7af55276f60888cb095aefb082bddc163
NghiTran <nghi.forwork05@gmail.com>
2025-03-19 02:55:17 +0700

    chatbox upload

--- Workspace/App/client/src/Components/Chatbox/Chatbox.jsx
+++ Workspace/App/client/src/Components/Chatbox/Chatbox.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import "./chatbox.css";
+import { motion } from "framer-motion"; //npm install framer-motion
+
 const Chatbox = () => {
     return (  
-        <h1>Chatbox</h1>
+        <div className="title">
+            <motion.h1
+                className="text-6xl font-bold"
+                animate={{ backgroundPosition: "200% 0" ,opacity: [0.5, 1, 10] }}
+                transition={{ duration: 3, repeat: Infinity, ease: "linear" }}
+                style={{
+                    fontSize: "2rem",
+                    fontWeight: "900",
+                    background: "linear-gradient(90deg, #ff4b2b,rgb(253, 126, 8) ,rgb(252, 213, 41))",
+                    backgroundSize: "200% 100%",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                }}
+                >
+                🔥 [SMART] To do list - Chatbox 🔥
+                </motion.h1>
+        </div>
     );
 }
  
