393e9f32e7477246c314469611484cb8d4187d0a
Nghia2123 <23521018@gm.uit.edu.vn>
2025-03-19 16:45:13 +0700

    update Nav.css to module.css

--- Workspace/App/client/src/Components/Navbar/Nav.jsx
+++ Workspace/App/client/src/Components/Navbar/Nav.jsx
@@ -1,4 +1,4 @@
-import "./Nav.css";
+import navs from "./navstyle.module.css";
 import { Link, useLocation } from "react-router-dom"; 
 import { icons } from "../../assets/icon";
 import threebears from "../../assets/threebears.jpg";
@@ -6,33 +6,33 @@ const Navbar = () => {
     const menu_icon = icons["menu"];
     return (  
         <>  
-            <nav className="nav-mobile">
-                <Link to="#" className="menu-button" onClick={openMenu}>
-                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            <nav className={navs.navmobile}>
+                <Link to="#" className={navs.menubutton} onClick={openMenu}>
+                    <svg className={navs.icon} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
                 </Link>
             </nav>
-            <nav className="nav">
-                <div className="site-title">
+            <nav className={navs.nav}>
+                <div className={navs.sitetitle}>
                     <img src={threebears} alt="logo"/>
-                    <Link to="/" className="appname">Calendar</Link>
+                    <Link to="/" className={navs.appname}>Calendar</Link>
             
                 </div>
-                <ul className="main-item">
+                <ul className={navs.mainitem}>
                     <CustomLink to="/Schedule" icon="date">Schedule</CustomLink>
                     <CustomLink to="/Chatbox" icon="chat">Chatbox</CustomLink>
                     <CustomLink to="/Report" icon="report">Report</CustomLink>
                 </ul>
-                <div className="tag" id="account">
-                    <div className="title">My Account</div>
-                    <ul className="account-menu">
+                <div className={`${navs.tag} ${navs.account}`}>
+                    <div className={navs.title}>My Account</div>
+                    <ul className={navs.accountmenu}>
                         <CustomLink to="/Mytask" icon="task">My Task</CustomLink>
                         <CustomLink to="/Myteam" icon="team">My Team</CustomLink>
                         <CustomLink to="/Myactivities" icon="activity">My Activities</CustomLink>
                     </ul>
                 </div>
-                <div className="tag" id="system">
-                    <div className="title">System</div>
-                    <ul className="system-menu">
+                <div className={`${navs.tag} ${navs.system}`}>
+                    <div className={navs.title}>System</div>
+                    <ul className={navs.systemmenu}>
                         <CustomLink to="/Setting" icon="setting">Setting</CustomLink>
                         <CustomLink to="/Logout" icon="logout">Logout</CustomLink>
                     </ul>
@@ -47,9 +47,9 @@ function CustomLink({to, icon, children, ...props}) {
     const isActive = location.pathname === to;
     const Icon = icons[icon];
     return (
-        <Link to={to} {...props} className="link">
-            <li className={`select ${isActive ? 'active' : ''}`}>
-                    {Icon && <Icon className="icon"/>}
+        <Link to={to} {...props} className={navs.link}>
+            <li className={`${navs.select} ${isActive ? navs.active : ""}`}>
+                    {Icon && <Icon className={navs.icon}/>}
                     {children}
             </li>
         </Link>
@@ -57,7 +57,7 @@ function CustomLink({to, icon, children, ...props}) {
 }
 
 function openMenu() {
-    document.querySelector(".nav").classList.toggle("show");
+    document.querySelector(`.${navs.nav}`).classList.toggle(navs.show);
 }
  
 export default Navbar;
