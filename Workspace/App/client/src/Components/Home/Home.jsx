6f8312d4a87985ab7a7ab93c9119ebbb6d108902
NghiTran <nghi.forwork05@gmail.com>
2025-03-21 11:26:34 +0700

    homepage

--- Workspace/App/client/src/Components/Home/Home.jsx
+++ Workspace/App/client/src/Components/Home/Home.jsx
@@ -1,39 +1,29 @@
-import "./Home.css";
+import home from "./Home.module.css"
 import DateContainer from "../miniCalendar/DateContainer";
 import { Typography, Button, Container, Box, Paper } from "@mui/material";
 // npm install @mui/material @emotion/react @emotion/styled
-
+import gifAnimation from "../../assets/homepage_element.gif";
 const Home = () => {
     return ( 
-        <div className="Home_byNghi">
+        <div className={home.Home_byNghi}>
             {/* Mini Calendar */}
-            <div className="date-container">
+            <div className={home.date_container}>
                 <DateContainer/>
             </div>
 
             {/* Nội dung chính */}
+            <div className={home.title_container}>
+            <div className={home.title_box}>
+                <h1 className={home.title_text}>The Happier Workspace</h1>
+                <p className={home.subtitle_text}>Write. Plan. Collaborate. With a little help from AI.</p>
+                <button className={home.login_button}>Login</button>
+            </div>
+            </div>
+
+
             <div>
-            <Container maxWidth="md">
-                <Paper elevation={3} sx={{ padding: 1, textAlign: "center", borderRadius: 3 }}>
-                    <Box mt={5}>
-                        <Typography 
-                            variant="h2" 
-                            fontWeight="bold" 
-                            color="primary"
-                            sx={{ textShadow: "2px 2px 4px rgba(0,0,0,0.2)" }}
-                        >
-                            The happier workspace
-                        </Typography>
-                        <Typography variant="body1" color="textSecondary" sx={{ mt: 2 }}>
-                            UsabilityHub is a remote user research platform that takes the
-                            guesswork out of design decisions by validating them with real users.
-                        </Typography>
-                        <Button variant="contained" color="primary" size="large" sx={{ mt: 4, borderRadius: 2 }}>
-                            Get Started
-                        </Button>
-                    </Box>
-                </Paper>
-            </Container>
+            <img src={gifAnimation} alt="GIF Animation" width="900px" />
+
             </div>
         </div>
         
