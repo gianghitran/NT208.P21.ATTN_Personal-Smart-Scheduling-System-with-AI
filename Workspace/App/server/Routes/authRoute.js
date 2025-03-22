9b5e85c12460dec86073766db320c4039e455695
Nghia2123 <23521018@gm.uit.edu.vn>
2025-03-22 13:38:57 +0700

    Refresh token in progress

--- Workspace/App/server/Routes/authRoute.js
+++ Workspace/App/server/Routes/authRoute.js
@@ -5,5 +5,6 @@ const route = require('express').Router();
 route.post('/register', authController.registerUser);
 route.post('/login', authController.loginUser);
 route.post('/logout', authController.logoutUser);
+route.post('/refresh', authController.requestRefreshToken);
 
 module.exports = route;
