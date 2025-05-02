f1e969c35ac53bb56df10d801e31142619a34ac7
NghiTran <nghi.forwork05@gmail.com>
2025-05-02 20:04:44 +0700

    final speechtotext feature

--- Workspace/App/server/Routes/SpeechtoTextRoute.js
+++ Workspace/App/server/Routes/SpeechtoTextRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { sendRecordtoAPI } = require("../Controllers/SpeechtoText/speedtotextController"); // Ensure correct import
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
 
-router.post("/send/:userId", sendRecordtoAPI);
+router.post("/send/:userId", upload.single('file'), sendRecordtoAPI);
 
 module.exports = router;
